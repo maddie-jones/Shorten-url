831b383c21dea23476640b2ba2ff7f8861be9c75
Maddie Jones <maddyg91@gmail.com>
2019-06-28 20:28:36 -0600

    Adds hashid generator to link

--- lib/shorten_api/links/link.ex
+++ lib/shorten_api/links/link.ex
@@ -13,8 +13,19 @@ defmodule ShortenApi.Links.Link do
   def changeset(link, attrs) do
     link
     |> cast(attrs, [:hash, :url])
+    |> generate_hash(attrs)
     |> validate_required([:hash, :url])
     |> unique_constraint(:hash)
     |> unique_constraint(:url)
   end
+
+  defp generate_hash(%__MODULE__{} = link, %{"hash" => ""}), do: generate_hash(link, %{hash: nil})
+
+  defp generate_hash(%__MODULE__{id: id} = link, %{"hash" => nil}) do
+    hashids = Hashids.new(salt: "this is my salt")
+    new_id = Hashids.encode(hashids, [id])
+    Map.put(link, :hash, new_id)
+  end
+
+  defp generate_hash(%__MODULE__{} = link, %{"hash" => hash}), do: link
 end
